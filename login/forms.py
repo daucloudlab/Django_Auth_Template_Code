277c4b124de6c21245871f978cec1bf4dfaa7dfd
Daulet Baimbetov <daucloudlab@gmail.com>
2015-07-10 20:54:07 +0600

    Django auth template code

--- login/forms.py
+++ login/forms.py
@@ -1,24 +1,23 @@
-import re
+# -*- coding: utf-8 -*-
 from django import forms
 from django.contrib.auth.models import User
-from django.utils.translation import ugettext_lazy as _
- 
+
 class RegistrationForm(forms.Form):
  
-    username = forms.RegexField(regex=r'^\w+$', widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=_("Username"), error_messages={ 'invalid': _("This value must contain only letters, numbers and underscores.") })
-    email = forms.EmailField(widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=_("Email address"))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)), label=_("Password"))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)), label=_("Password (again)"))
+    username = forms.RegexField(regex=r'^\w+$', widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label="Логин", error_messages={ 'invalid': "Тек әріптер мен сандардан тұрады" })
+    email = forms.EmailField(widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label="email")
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)), label="Құпия сөз")
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=30, render_value=False)), label="Құпия сөз (қайтала)")
  
     def clean_username(self):
         try:
             user = User.objects.get(username__iexact=self.cleaned_data['username'])
         except User.DoesNotExist:
             return self.cleaned_data['username']
-        raise forms.ValidationError(_("The username already exists. Please try another one."))
+        raise forms.ValidationError("Мұндай қолданушы тіркелген.")
  
     def clean(self):
         if 'password1' in self.cleaned_data and 'password2' in self.cleaned_data:
             if self.cleaned_data['password1'] != self.cleaned_data['password2']:
-                raise forms.ValidationError(_("The two password fields did not match."))
+                raise forms.ValidationError("Құпия сөздер сәйкес келмейді.")
         return self.cleaned_data
